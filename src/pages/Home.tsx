dad0a0d6d0814224975576a899691d7591082526
Mike Morano <mmorano@mikeandwan.us>
2023-02-20 20:48:41 -0500

    add way to configure different environments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
 import { Component } from "solid-js";
 
-import HomeToolbar from '../components/HomeToolbar';
+import HomeToolbar from "../components/HomeToolbar";
 
 const Home: Component = () => {
     return (
-        <div class="grid
+        <div
+            class="grid
                     grid-rows-[max-content_auto] grid-cols-[100vw]
-                    md:grid-rows-[100vh] md:grid-cols-[max-content_auto]">
+                    md:grid-rows-[100vh] md:grid-cols-[max-content_auto]"
+        >
             <HomeToolbar />
             <h1>Home</h1>
+            <p>Here is a variable: {import.meta.env.VITE_AUTH_CLIENT_ID}</p>
         </div>
     );
 };
