5487e5f50f68bc46686355f6e64000bf43e9379d
Mike Morano <mmorano@mikeandwan.us>
2023-03-04 06:53:03 -0500

    update app to redirect to desired landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import type { Component } from "solid-js";
-import { useRoutes } from "@solidjs/router";
+import { Component, createEffect } from "solid-js";
+import { useLocation, useNavigate, useRoutes } from "@solidjs/router";
 
 import PrimaryNav from "./components/primary-nav/PrimaryNav";
 import { appRoutes } from "./routes";
 
 const App: Component = () => {
     const Routes = useRoutes(appRoutes);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    createEffect(() => {
+        if(location.pathname === "/") {
+            navigate("/categories", { replace: true });
+        }
+    });
 
     return (
         <div class="grid
