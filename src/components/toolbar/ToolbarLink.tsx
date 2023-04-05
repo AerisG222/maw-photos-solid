426d2698447a50ef94df4dcc8f1f6698299dc741
Mike Morano <mmorano@mikeandwan.us>
2023-04-05 19:49:32 -0400

    draw over parent's border when active

--- src/components/toolbar/ToolbarLink.tsx
+++ src/components/toolbar/ToolbarLink.tsx
@@ -11,9 +11,9 @@ const ToolbarLink: Component<Props> = (props) => {
         <A
             href={props.route.path}
             end={true}
-            activeClass="color-primary-content bg-primary"
+            activeClass="color-primary-content bg-primary m-r[-1px]"
             inactiveClass="color-primary"
-            class="px-3 py-1 hover:color-primary-content hover:bg-primary"
+            class="px-3 py-1 hover:color-primary-content hover:bg-primary hover:m-r[-1px]"
             title={props.route.name}
         >
             <span class={`text-6 ${props.route.icon}`} />
