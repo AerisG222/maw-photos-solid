208dcfbb297d7943edb2576d2be0f5deda13c832
Mike Morano <mmorano@mikeandwan.us>
2023-04-05 19:43:59 -0400

    a little more work on the nav panel borders

--- src/components/toolbar/ToolbarLayout.tsx
+++ src/components/toolbar/ToolbarLayout.tsx
@@ -4,7 +4,7 @@ const ToolbarLayout: ParentComponent = (props) => {
     const c = children(() => props.children);
 
     return (
-        <div class="flex md:flex-col bg-secondary-content:8 border-r-1 border-r-primary:50%">
+        <div class="flex md:flex-col bg-secondary-content:6 border-r-1 border-r-secondary-content:10%">
             {c()}
         </div>
     );
