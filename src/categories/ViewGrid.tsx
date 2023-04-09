30bed4f29a69e89e1ee1ab011dba5e2df676043c
Mike Morano <mmorano@mikeandwan.us>
2023-04-09 07:35:06 -0400

    more examples of how to pull out data

--- src/categories/ViewGrid.tsx
+++ src/categories/ViewGrid.tsx
@@ -10,6 +10,8 @@ const GridView: Component = () => {
     authGuard();
 
     const categories = getCategories(accessToken());
+    const count = () => categories?.data?.count;
+    const years = () => [...new Set(categories?.data?.items?.map(x => x.year))];
 
     createEffect(() => {
         if(categories.isSuccess) {
@@ -25,6 +27,8 @@ const GridView: Component = () => {
 
             <Suspense fallback={<p>Loading...</p>}>
                 <MainContent title="Categories - Grid">
+                    {count()}
+                    {years()}
                     <p>Here is a variable: {import.meta.env.VITE_AUTH_CLIENT_ID}</p>
                 </MainContent>
             </Suspense>
