4fd0f84b9fe994da3d20e7007982d23aee501802
Mike Morano <mmorano@mikeandwan.us>
2023-05-03 17:45:30 -0400

    use context to fully determine links

--- src/categories-photos/Toolbar.tsx
+++ src/categories-photos/Toolbar.tsx
@@ -1,29 +1,38 @@
 import { ParentComponent, Show } from 'solid-js'
-import { useParams } from '@solidjs/router';
 
 import { categoriesPhotosBulkEdit, categoriesPhotosDetail, categoriesPhotosFullscreen, categoriesPhotosGrid, categoriesPhotosMap } from './_routes';
+import { useCategoryContext } from '../contexts/CategoryContext';
+import { usePhotoListContext } from '../contexts/PhotoListContext';
 
 import Divider from '../components/layout/Divider';
 import ToolbarLayout from '../components/toolbar/ToolbarLayout';
 import ToolbarLink from '../components/toolbar/ToolbarLink';
 
 const Toolbar: ParentComponent = (props) => {
-    const categoryId = parseInt(useParams().categoryId);
+    const [categoryState] = useCategoryContext();
+    const [photoState] = usePhotoListContext();
+
     const c = () => props.children;
+    const getParams = () => ({
+        categoryId: categoryState.activeCategory?.id,
+        photoId: photoState.activePhoto?.id
+    });
 
     return (
-        <ToolbarLayout>
-            <ToolbarLink route={categoriesPhotosGrid}       routeParams={{categoryId: categoryId}} />
-            <ToolbarLink route={categoriesPhotosDetail}     routeParams={{categoryId: categoryId}} />
-            <ToolbarLink route={categoriesPhotosFullscreen} routeParams={{categoryId: categoryId}} />
-            <ToolbarLink route={categoriesPhotosMap}        routeParams={{categoryId: categoryId}} />
-            <ToolbarLink route={categoriesPhotosBulkEdit}   routeParams={{categoryId: categoryId}} />
+        <Show when={categoryState.activeCategory}>
+            <ToolbarLayout>
+                <ToolbarLink route={categoriesPhotosGrid}       routeParams={getParams()} />
+                <ToolbarLink route={categoriesPhotosDetail}     routeParams={getParams()} />
+                <ToolbarLink route={categoriesPhotosFullscreen} routeParams={getParams()} />
+                <ToolbarLink route={categoriesPhotosMap}        routeParams={getParams()} />
+                <ToolbarLink route={categoriesPhotosBulkEdit}   routeParams={getParams()} />
 
-            <Show when={!!c()}>
-                <Divider />
-                {c()}
-            </Show>
-        </ToolbarLayout>
+                <Show when={!!c()}>
+                    <Divider />
+                    {c()}
+                </Show>
+            </ToolbarLayout>
+        </Show>
     );
 };
 
