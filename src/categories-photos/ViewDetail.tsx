94ac90adaee53d48a6a076197898fe99ccff670e
Mike Morano <mmorano@mikeandwan.us>
2023-05-05 17:13:04 -0400

    allow main image to scale based on available space and size of photo list and breadcrumbs

--- src/categories-photos/ViewDetail.tsx
+++ src/categories-photos/ViewDetail.tsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from '@solidjs/router';
 import { categoriesPhotosDetail, getPhotoCategoryRoutePath } from './_routes';
 import { usePhotoDetailViewSettingsContext } from '../contexts/PhotoDetailViewSettingsContext';
 import { usePhotoListContext } from '../contexts/PhotoListContext';
+import { getThumbnailSize } from '../models/ThumbnailSize';
 
 import DetailToolbar from './ToolbarDetail';
 import Toolbar from "./Toolbar";
@@ -30,15 +31,26 @@ const ViewDetail: Component = () => {
         navigate(getPhotoCategoryRoutePath(categoriesPhotosDetail, p.categoryId, p.id));
     }
 
+    const getMaxHeight = () => {
+        let reservedHeight = 0;
+
+        reservedHeight += settings.showBreadcrumbs ? 28 : 0;
+        reservedHeight += getThumbnailSize(settings.thumbnailSize).height;
+
+        return `max-height: calc(100vh - ${reservedHeight}px);`;
+    }
+
     return (
         <Layout toolbar={toolbar} sidebar={<Sidebar />}>
-            <div class="flex flex-col flex-[max-content_auto_max-content] h-100vh">
+            <div class="flex flex-col flex-[max-content_auto_max-content] h-100vh --val-[100px]">
                 <Show when={settings.showBreadcrumbs} fallback={<div />}>
                     <CategoryBreadcrumb />
                 </Show>
 
-                <div class="flex-1 mb-1">
-                    <img src={photoListState.activePhoto?.imageMd?.url} class="h-100% max-w-100% max-h-100% object-contain justify-self-center m-x-auto" />
+                <div class="flex flex-wrap flex-1 flex-justify-center flex-content-center">
+                    <img src={photoListState.activePhoto?.imageMd?.url}
+                        class="h-100% max-w-100%"
+                        style={getMaxHeight()} />
                 </div>
 
                 <Show when={settings.showPhotoList} fallback={<div/>}>
--- src/models/ThumbnailSize.ts
+++ src/models/ThumbnailSize.ts
@@ -5,10 +5,10 @@ export type ThumbnailSizeIdType = string;
 export type ThumbnailSize = KeyValuePair<ThumbnailSizeIdType> & { klass: any };
 
 export const allThumbnailSizes: ThumbnailSize[] = [
-    { id: "default",   name: 'Default',    klass: { 'w-[160px]': true, 'h-[120px]': true } },
-    { id: "small",     name: 'Small',      klass: { 'w-[120px]': true, 'h-[90px]': true } },
-    { id: "verySmall", name: 'Very Small', klass: { 'w-[80px]': true,  'h-[60px]': true } },
-    { id: "tiny",      name: 'Tiny',       klass: { 'w-[40px]': true,  'h-[30px]': true } },
+    { id: "default",   name: 'Default',    width: 160, height: 120, klass: { 'w-[160px]': true, 'h-[120px]': true } },
+    { id: "small",     name: 'Small',      width: 120, height:  90, klass: { 'w-[120px]': true, 'h-[90px]':  true } },
+    { id: "verySmall", name: 'Very Small', width:  80, height:  60, klass: { 'w-[80px]':  true, 'h-[60px]':  true } },
+    { id: "tiny",      name: 'Tiny',       width:  40, height:  30, klass: { 'w-[40px]':  true, 'h-[30px]':  true } },
 ];
 
 export const allThumbnailClasses = new Set(allThumbnailSizes
@@ -18,6 +18,10 @@ export const allThumbnailClasses = new Set(allThumbnailSizes
 
 export const defaultGridThumbnailSize: ThumbnailSizeIdType = 'default';
 export const defaultListThumbnailSize: ThumbnailSizeIdType = 'verySmall';
+
+export const getThumbnailSize = (thumbnailSize: ThumbnailSizeIdType) => {
+    return allThumbnailSizes.find(x => equalsIgnoreCase(x.id, thumbnailSize)) ?? allThumbnailClasses[0];
+}
 
 export const getNextThumbnailSize = (thumbnailSize: ThumbnailSizeIdType) => {
     var idx = allThumbnailSizes.findIndex(x => equalsIgnoreCase(x.id, thumbnailSize));
