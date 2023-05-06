22a44f2c2b4dbdceb80a76affb5d0c5e030d162e
Mike Morano <mmorano@mikeandwan.us>
2023-05-06 16:47:27 -0400

    a bit more styling

--- src/categories-photos/components/InfoCard.tsx
+++ src/categories-photos/components/InfoCard.tsx
@@ -10,8 +10,9 @@ const InfoCard: ParentComponent<Props> = (props) => {
 
     return (
         <div class="bg-primaryContent rounded m-1 p-2 border-1 border-secondary-content:10%">
-            <div>
-                <span class={`text-6 ${props.icon}`} /> - {props.title}
+            <div class="color-accent">
+                <span class={`m-r-2 text-6 ${props.icon}`} />
+                <strong class="text-3.5">{props.title}</strong>
             </div>
 
             <div class="m-t-2">
