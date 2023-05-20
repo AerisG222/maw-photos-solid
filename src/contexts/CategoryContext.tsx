50e756bd141216b1e3e77f3cb762fb6d8d9314f3
Mike Morano <mmorano@mikeandwan.us>
2023-05-20 06:25:29 -0400

    move some chart stuff out of context

--- src/contexts/CategoryContext.tsx
+++ src/contexts/CategoryContext.tsx
@@ -6,6 +6,7 @@ import { VideoCategory } from '../models/api/VideoCategory';
 import { Category, ICategory } from '../models/Category';
 import { CategoryTypeFilterIdType, getCategoryTypeFilter } from '../models/CategoryTypeFilter';
 import { YearFilterIdType, yearFilterPredicate } from '../models/YearFilter';
+import { buildStatsData } from '../models/utils/ChartUtils';
 
 export type CategoryState = {
     readonly photoCategories: Category<PhotoCategory>[];
@@ -159,45 +160,6 @@ export const CategoryProvider: ParentComponent = (props) => {
         setActiveCategory(cat);
     };
 
-    // https://www.learnui.design/tools/data-color-picker.html
-    const chartColors = [
-        '#003f5c',
-        '#2f4b7c',
-        '#665191',
-        '#a05195',
-        '#d45087',
-        '#f95d6a',
-        '#ff7c43',
-        '#ffa600',
-
-        '#5c1f1f',
-        '#772c24',
-        '#913c27',
-        '#ab4d28',
-        '#c36126',
-        '#da7621',
-        '#ee8d18',
-        '#ffa600',
-
-        '#0e5c00',
-        '#346900',
-        '#537600',
-        '#728200',
-        '#938d00',
-        '#b69700',
-        '#da9f00',
-        '#ffa600',
-
-        '#05005c',
-        '#550062',
-        '#89005f',
-        '#b40056',
-        '#d61449',
-        '#ee4c38',
-        '#fc7a22',
-        '#ffa600',
-    ];
-
     const setActiveVideoCategory = (categoryId: number) => setActiveCategory(state.videoCategories.find(x => x.id === categoryId));
 
     const getPhotoStatsChartData = (valueFunc: (cat: PhotoCategory) => number) =>
@@ -208,37 +170,6 @@ export const CategoryProvider: ParentComponent = (props) => {
 
     const getCombinedStatsChartData = (valueFunc: (cat: Category) => number) =>
         buildStatsData(getAllYears(), getAllCategories().map(x => x.actual), valueFunc);
-
-    const buildStatsData = (years: number[], categories: PhotoCategory[] | VideoCategory[], valueFunc: (Category) => number) => {
-        const result = [];
-
-        for(const year of years) {
-            const yearId = `year-${year}`;
-            const categoriesInYear = categories.filter(x => x.year === year);
-
-            const yearPoint = {
-                id: yearId,
-                name: year.toString(),
-                value: 0,
-                color: chartColors[year % chartColors.length]
-            };
-
-            result.push(yearPoint);
-
-            for(const cat of categoriesInYear) {
-                result.push({
-                    id: `year-${year}-${cat.id}`,
-                    parent: yearId,
-                    name: cat.name,
-                    value: valueFunc(cat)
-                })
-
-                yearPoint.value += valueFunc(cat);
-            }
-        }
-
-        return result;
-    }
 
     return (
         <CategoryContext.Provider value={[state, {
