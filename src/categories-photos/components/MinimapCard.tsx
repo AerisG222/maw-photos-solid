74376d184f0f0938d267cc067240406497e2904a
Mike Morano <mmorano@mikeandwan.us>
2023-06-14 18:04:55 -0400

    initial work to load google maps

--- src/categories-photos/components/MinimapCard.tsx
+++ src/categories-photos/components/MinimapCard.tsx
@@ -1,9 +1,26 @@
-import { Component } from 'solid-js';
+import { Component, onMount } from 'solid-js';
 
 const MinimapCard: Component = () => {
+    let el: HTMLDivElement | undefined;
+    let map: google.maps.Map;
+
+    async function initMap(): Promise<void> {
+        const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+
+        if(el) {
+            map = new Map(el, {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8,
+            });
+        }
+    }
+
+    onMount(() => {
+        initMap();
+    });
 
     return (
-        <div>minimap</div>
+        <div class="h-[320px] w-[100%]" ref={el} />
     );
 }
 
