ff9919ba11ab0d7870e7c9c50b7007be21a6e349
Mike Morano <mmorano@mikeandwan.us>
2023-06-18 11:28:14 -0400

    show image in fullscreen

--- src/categories-photos/ViewFullscreen.tsx
+++ src/categories-photos/ViewFullscreen.tsx
@@ -1,6 +1,7 @@
 import { Component, onCleanup } from "solid-js";
 
 import { useFullscreenContext } from '../contexts/FullscreenContext';
+import { usePhotoListContext } from '../contexts/PhotoListContext';
 
 import FullscreenToolbar from './ToolbarFullscreen';
 import Toolbar from "./Toolbar";
@@ -8,6 +9,7 @@ import Layout from '../components/layout/Layout';
 
 const ViewFullscreen: Component = () => {
     const [fullscreen, { setFullscreen }] = useFullscreenContext();
+    const [photoListState] = usePhotoListContext();
 
     setFullscreen(true);
     onCleanup(() => setFullscreen(false));
@@ -19,8 +21,12 @@ const ViewFullscreen: Component = () => {
     );
 
     return (
-        <Layout toolbar={toolbar} title="Photo Categories Fullscreen">
-
+        <Layout toolbar={toolbar}>
+            <div class="grid h-[100vh] w-[100%] justify-center">
+                <img
+                    src={photoListState.activePhoto?.imageMd?.url}
+                    class="h-100% max-w-100% object-contain" />
+            </div>
         </Layout>
     );
 };
