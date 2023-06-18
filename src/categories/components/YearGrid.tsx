5098e20cce92378d2056de24e616f4c8b8ebd2a9
Mike Morano <mmorano@mikeandwan.us>
2023-06-18 10:08:29 -0400

    only show titles for large thumbnails

--- src/categories/components/YearGrid.tsx
+++ src/categories/components/YearGrid.tsx
@@ -3,6 +3,7 @@ import { Component, For } from 'solid-js';
 import CategoryCard from './CategoryCard';
 import { useCategoryGridViewSettingsContext } from '../../contexts/CategoryGridViewSettingsContext';
 import { ICategory } from '../../models/Category';
+import { defaultGridThumbnailSize } from '../../models/ThumbnailSize';
 
 export type Props = {
     year: number;
@@ -18,7 +19,7 @@ const YearGrid: Component<Props> = (props) => {
 
             <div class="flex flex-gap-2 flex-wrap place-content-center mb-4">
                 <For each={props.categories}>{ category =>
-                    <CategoryCard category={category} showTitles={settings.showTitles} thumbnailSize={settings.thumbnailSize} />
+                    <CategoryCard category={category} showTitles={settings.showTitles && settings.thumbnailSize === defaultGridThumbnailSize} thumbnailSize={settings.thumbnailSize} />
                 }</For>
             </div>
         </>
