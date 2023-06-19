c08ac2ea553c215115d0a2639bd69f40225f2236
Mike Morano <mmorano@mikeandwan.us>
2023-06-19 17:17:34 -0400

    move category loading to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,32 @@
-import { Component, Show, createEffect, createResource } from "solid-js";
+import { Component, Show } from "solid-js";
 import { useRoutes } from "@solidjs/router";
 
 import { appRoutes } from "./routes";
 import { useAppSettingsContext } from './contexts/AppSettingsContext';
-import { isLoggedIn } from './auth/auth';
-import { getPhotoCategories } from './api/PhotoCategories';
-import { getVideoCategories } from './api/VideoCategories';
-import { useCategoryContext } from './contexts/CategoryContext';
+import { useFullscreenContext } from './contexts/FullscreenContext';
 
 import PrimaryNav from "./components/primary-nav/PrimaryNav";
-import { useFullscreenContext } from './contexts/FullscreenContext';
+import CategoryLoader from './CategoryLoader';
 
 const App: Component = () => {
     const Routes = useRoutes(appRoutes);
     const [appSettings] = useAppSettingsContext();
     const [fullscreen] = useFullscreenContext();
-    const [categoryState, { setPhotoCategories, setVideoCategories }] = useCategoryContext();
-
-    const getPhotoCats = (isLoggedIn) => isLoggedIn ? getPhotoCategories() : null;
-    const getVideoCats = (isLoggedIn) => isLoggedIn ? getVideoCategories() : null;
-
-    const [photoCategories] = createResource(isLoggedIn, getPhotoCats);
-    const [videoCategories] = createResource(isLoggedIn, getVideoCats);
-
-    createEffect(() => {
-        setPhotoCategories(photoCategories()?.items ?? []);
-        setVideoCategories(videoCategories()?.items ?? []);
-    });
 
     return (
-        <div data-theme={appSettings.theme}
-             class="grid
-                  grid-rows-[max-content_auto] grid-cols-[100vw]
-                  md:grid-rows-[100vh] md:grid-cols-[max-content_auto]">
+        <CategoryLoader>
+            <div data-theme={appSettings.theme}
+                class="grid
+                    grid-rows-[max-content_auto] grid-cols-[100vw]
+                    md:grid-rows-[100vh] md:grid-cols-[max-content_auto]">
 
-            <Show when={!fullscreen.isFullscreen} fallback={<div class="w-0" />}>
-                <PrimaryNav />
-            </Show>
+                <Show when={!fullscreen.isFullscreen} fallback={<div class="w-0" />}>
+                    <PrimaryNav />
+                </Show>
 
-            <Routes />
-        </div>
+                <Routes />
+            </div>
+        </CategoryLoader>
     );
 };
 
---
+++ src/CategoryLoader.tsx
@@ -0,0 +1,29 @@
+import { ParentComponent, createEffect, createResource } from 'solid-js';
+
+import { isLoggedIn } from './auth/auth';
+import { getPhotoCategories } from './api/PhotoCategories';
+import { getVideoCategories } from './api/VideoCategories';
+import { useCategoryContext } from './contexts/CategoryContext';
+
+const CategoryLoader: ParentComponent = (props) => {
+    const [categoryState, { setPhotoCategories, setVideoCategories }] = useCategoryContext();
+
+    const getPhotoCats = (isLoggedIn) => isLoggedIn ? getPhotoCategories() : null;
+    const getVideoCats = (isLoggedIn) => isLoggedIn ? getVideoCategories() : null;
+
+    const [photoCategories] = createResource(isLoggedIn, getPhotoCats);
+    const [videoCategories] = createResource(isLoggedIn, getVideoCats);
+
+    createEffect(() => {
+        setPhotoCategories(photoCategories()?.items ?? []);
+        setVideoCategories(videoCategories()?.items ?? []);
+    });
+
+    return (
+        <>
+            {props.children}
+        </>
+    )
+}
+
+export default CategoryLoader;
