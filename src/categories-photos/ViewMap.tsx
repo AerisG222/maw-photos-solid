bfa4981594ba05e84b24c22e8b73208ba924775f
Mike Morano <mmorano@mikeandwan.us>
2023-06-20 21:23:48 -0400

    start to work on the map view

--- src/categories-photos/ViewMap.tsx
+++ src/categories-photos/ViewMap.tsx
@@ -1,19 +1,79 @@
-import { Component } from "solid-js";
+import { Component, createEffect, createSignal, onMount } from "solid-js";
 
 import MapToolbar from './ToolbarMap';
 import Toolbar from "./Toolbar";
 import Layout from '../components/layout/Layout';
+import { usePhotoMapViewSettingsContext } from '../contexts/settings/PhotoMapViewSettingsContext';
+import { usePhotoListContext } from '../contexts/PhotoListContext';
 
 const ViewMap: Component = () => {
+    const [state, { setMapType, setZoom }] = usePhotoMapViewSettingsContext();
+    const [photoListState] = usePhotoListContext();
+    const [initialized, setInitialized] = createSignal(false);
+    let el: HTMLDivElement | undefined;
+
     const toolbar = (
         <Toolbar>
             <MapToolbar />
         </Toolbar>
     );
 
-    return (
-        <Layout toolbar={toolbar} title="Photo Categories Map">
+    const defaultMapOptions = {
+        controlSize: 24,
+        center: { lat: 0, lng: 0 },
+        fullscreenControl: true,
+        mapTypeControl: true,
+        mapId: 'af11584565f27198',
+        mapTypeId: state.mapType,
+        zoom: state.zoom
+    };
 
+    let map: google.maps.Map;
+    const markers: google.maps.AdvancedMarkerElement[] = [];
+
+    async function initMap(): Promise<void> {
+        const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+        if(el) {
+            map = new Map(el, defaultMapOptions);
+            map.addListener('zoom_changed', () => setZoom(map.getZoom()));
+            map.addListener('maptypeid_changed', () => setMapType(map.getMapTypeId()));
+
+            for(const photo of photoListState.photos) {
+                if(photo.latitude && photo.longitude) {
+                    markers.push(new AdvancedMarkerElement({map, position: { lat: photo.latitude, lng: photo.longitude}}));
+                }
+            }
+
+            setInitialized(true);
+        }
+    }
+
+    const updateMap = () => {
+        if(photoListState.activePhoto?.latitude && photoListState.activePhoto?.longitude) {
+            const pos = {
+                lat: photoListState.activePhoto?.latitude,
+                lng: photoListState.activePhoto?.longitude
+            };
+
+            map.setCenter(pos);
+        }
+    }
+
+    onMount(() => {
+        initMap();
+    });
+
+    createEffect(() => {
+        if(initialized()) {
+            updateMap();
+        }
+    });
+
+    return (
+        <Layout toolbar={toolbar}>
+            <div class="h-[100vh] w-[100%]" ref={el} />
         </Layout>
     );
 };
