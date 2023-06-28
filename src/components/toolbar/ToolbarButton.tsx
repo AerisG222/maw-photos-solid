ca96404805d71de8ab9767b362761e66ea745026
Mike Morano <mmorano@mikeandwan.us>
2023-06-28 07:46:21 -0400

    apply the rotation to the icon so button dimensions are consistent regardless of rotation

--- src/components/toolbar/ToolbarButton.tsx
+++ src/components/toolbar/ToolbarButton.tsx
@@ -26,11 +26,11 @@ const ToolbarButton: Component<Props> = (props) => {
         <button
             disabled={props.disabled}
             class="px-3 py-1 hover:bg-accent hover:color-accentContent disabled:bg-transparent! disabled:color-secondary-content disabled:opacity-20"
-            classList={{"rotate-90": props.rotate90, "bg-accent": props.active, "color-accentContent": props.active}}
+            classList={{"bg-accent": props.active, "color-accentContent": props.active}}
             title={props.name}
             onClick={[handleClick, null]}
         >
-            <span class={`text-6 ${props.icon}`} />
+            <span class={`text-6 ${props.icon}`} classList={{"rotate-90": props.rotate90}} />
         </button>
     );
 };
