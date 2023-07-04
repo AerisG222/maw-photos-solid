1e68c3aababc9dc0cb9f1121258ea808773f25c5
Mike Morano <mmorano@mikeandwan.us>
2023-07-04 10:56:03 -0400

    start to adjust toolbar for category type

--- src/components/media/Toolbar.tsx
+++ src/components/media/Toolbar.tsx
@@ -1,34 +1,48 @@
-import { ParentComponent, Show, children } from 'solid-js'
+import { Match, ParentComponent, Show, Switch, children } from 'solid-js'
 
 import { categoriesPhotosBulkEdit, categoriesPhotosDetail, categoriesPhotosFullscreen, categoriesPhotosGrid, categoriesPhotosMap } from '../../categories-photos/_routes';
 import { useCategoryContext } from '../../contexts/CategoryContext';
 import { useMediaListContext } from '../../contexts/MediaListContext';
+import { usePhotoPageSettingsContext } from '../../contexts/settings/PhotoPageSettingsContext';
 
 import Divider from '../layout/Divider';
 import ToolbarLayout from '../toolbar/ToolbarLayout';
 import ToolbarLink from '../toolbar/ToolbarLink';
-import { usePhotoPageSettingsContext } from '../../contexts/settings/PhotoPageSettingsContext';
+import { MediaTypePhoto, MediaTypeVideo } from '../../models/Media';
+import { categoriesVideosDetail } from '../../categories-videos/_routes';
 
 const Toolbar: ParentComponent = (props) => {
     const [categoryState] = useCategoryContext();
-    const [photoState] = useMediaListContext();
+    const [mediaList] = useMediaListContext();
     const [settings, { setViewMode }] = usePhotoPageSettingsContext();
 
     const c = children(() => props.children);
 
-    const getParams = () => ({
+    const getPhotoParams = () => ({
         categoryId: categoryState.activeCategory?.id,
-        photoId: photoState.activeItem?.id
+        photoId: mediaList.activeItem?.id,
+    });
+
+    const getVideoParams = () => ({
+        categoryId: categoryState.activeCategory?.id,
+        videoId: mediaList.activeItem?.id,
     });
 
     return (
         <Show when={categoryState.activeCategory}>
             <ToolbarLayout>
-                <ToolbarLink route={categoriesPhotosGrid}       routeParams={getParams()} clickHandler={() => setViewMode("grid")}/>
-                <ToolbarLink route={categoriesPhotosDetail}     routeParams={getParams()} clickHandler={() => setViewMode("detail")}/>
-                <ToolbarLink route={categoriesPhotosFullscreen} routeParams={getParams()} clickHandler={() => setViewMode("fullscreen")}/>
-                <ToolbarLink route={categoriesPhotosMap}        routeParams={getParams()} clickHandler={() => setViewMode("map")}/>
-                <ToolbarLink route={categoriesPhotosBulkEdit}   routeParams={getParams()} clickHandler={() => setViewMode("bulkEdit")}/>
+                <Switch>
+                    <Match when={categoryState.activeCategory.type === MediaTypePhoto}>
+                        <ToolbarLink route={categoriesPhotosGrid}       routeParams={getPhotoParams()} clickHandler={() => setViewMode("grid")}/>
+                        <ToolbarLink route={categoriesPhotosDetail}     routeParams={getPhotoParams()} clickHandler={() => setViewMode("detail")}/>
+                        <ToolbarLink route={categoriesPhotosFullscreen} routeParams={getPhotoParams()} clickHandler={() => setViewMode("fullscreen")}/>
+                        <ToolbarLink route={categoriesPhotosMap}        routeParams={getPhotoParams()} clickHandler={() => setViewMode("map")}/>
+                        <ToolbarLink route={categoriesPhotosBulkEdit}   routeParams={getPhotoParams()} clickHandler={() => setViewMode("bulkEdit")}/>
+                    </Match>
+                    <Match when={categoryState.activeCategory.type === MediaTypeVideo}>
+                        <ToolbarLink route={categoriesVideosDetail}     routeParams={getVideoParams()} clickHandler={() => setViewMode("detail")}/>
+                    </Match>
+                </Switch>
 
                 <Show when={!!c()}>
                     <Divider />
