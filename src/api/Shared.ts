e2e89824f4f79456361ecd2f37877fc0b4a54909
Mike Morano <mmorano@mikeandwan.us>
2023-07-13 13:35:18 -0400

    minor formatting

--- src/api/Shared.ts
+++ src/api/Shared.ts
@@ -4,13 +4,13 @@ const buildAbsoluteUrl = (relativeUrl: string): string =>
     `${import.meta.env.VITE_API_URI}/${relativeUrl}`;
 
 export const queryMawApi = async <T>(relativeUrl: string) =>
-    (await callMawApi('GET', relativeUrl, undefined)).json() as T
+    (await callMawApi('GET', relativeUrl, undefined)).json() as T;
 
 export const patchMawApi = (relativeUrl: string, content: any) =>
     callMawApi('PATCH', relativeUrl, content);
 
 export const postMawApi = (relativeUrl: string, content: any) =>
-    callMawApi('POST', relativeUrl, content)
+    callMawApi('POST', relativeUrl, content);
 
 const callMawApi = async (method: string, relativeUrl: string, content: any) =>
     await fetch(
