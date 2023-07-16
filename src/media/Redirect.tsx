f05f8b2a8ee36caf0d3c45fa1b15025f0bb440c6
Mike Morano <mmorano@mikeandwan.us>
2023-07-16 09:38:56 -0400

    fix the media redirect

--- src/media/Redirect.tsx
+++ src/media/Redirect.tsx
@@ -3,17 +3,17 @@ import { Component } from "solid-js";
 
 import { usePhotoPageSettingsContext } from '../contexts/settings/PhotoPageSettingsContext';
 import { MediaView, getMediaPathByView } from './_routes';
-import { CategoryTypePhotos } from '../_models/CategoryType';
+import { CategoryType } from '../_models/CategoryType';
 
-const PhotoCategoriesRedirect: Component = () => {
+const Redirect: Component = () => {
     const [settings] = usePhotoPageSettingsContext();
     const navigate = useNavigate();
     const params = useParams();
     const categoryId = parseInt(params.categoryId);
 
-    navigate(getMediaPathByView(settings.viewMode as MediaView, CategoryTypePhotos, categoryId));
+    navigate(getMediaPathByView(settings.viewMode as MediaView, params.categoryType as CategoryType, categoryId));
 
     return <></>;
 };
 
-export default PhotoCategoriesRedirect;
+export default Redirect;
