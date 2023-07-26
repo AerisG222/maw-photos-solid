182e322b03354e89afa4504da39067d3a45b5482
Mike Morano <mmorano@mikeandwan.us>
2023-07-26 10:33:19 -0400

    dynamically populate icon safe list once again

--- unocss.config.ts
+++ unocss.config.ts
@@ -4,31 +4,11 @@ import { presetUno } from '@unocss/preset-uno';
 import { presetIcons } from '@unocss/preset-icons';
 import { presetDaisy } from 'unocss-preset-daisy';
 
-//import { allRouteIcons } from './src/routes';
+import { allRouteIcons } from './src/routes';
 import { allThemes } from './src/_models/Theme';
 import { allThumbnailClasses } from './src/_models/ThumbnailSize';
 import { allMarginClasses } from './src/_models/Margin';
 import { categoryTypeIcons } from './src/_models/CategoryTypes';
-
-// uh oh - now we have the same issue for route icons...
-const allRouteIcons = [
-    "i-ic-round-help-outline",
-    "i-ic-outline-apps",
-    "i-ic-round-dashboard",
-    "i-ic-round-fullscreen",
-    "i-ic-round-map",
-    "i-ic-round-collections",
-    "i-ic-round-format-list-bulleted",
-    "i-ic-round-home",
-    "i-ic-round-search",
-    "i-ic-round-image",
-    "i-ic-round-videocam",
-    "i-ic-round-shuffle",
-    "i-ic-baseline-settings",
-    "i-ic-outline-photo-camera",
-    "i-ic-round-functions",
-    "i-ic-round-bar-chart"
-];
 
 export default defineConfig<Theme>({
     presets: [
