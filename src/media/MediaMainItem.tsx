a8f64a10140868eaf13c02bbc051250f41c766fe
Mike Morano <mmorano@mikeandwan.us>
2023-07-27 10:22:40 -0400

    handle case when media not defined

--- src/media/MediaMainItem.tsx
+++ src/media/MediaMainItem.tsx
@@ -1,4 +1,4 @@
-import { Component, Match, Switch } from 'solid-js';
+import { Component, Match, Show, Switch } from 'solid-js';
 
 import { Media, MediaTypePhoto, MediaTypeVideo, Photo, Video } from '../_models/Media';
 import { useVisualEffectsContext } from './contexts/VisualEffectsContext';
@@ -15,18 +15,20 @@ const MediaMainItem: Component<Props> = (props) => {
     const [, { getFilterStyles, getTransformStyles }] = useVisualEffectsContext();
 
     return (
-        <div
-            class="h-100% w-100% max-h-100vh max-w-100% object-contain self-center"
-            style={`${props.maxHeightStyle ?? ''} ${getTransformStyles()} ${getFilterStyles()}`}>
-            <Switch>
-                <Match when={props.media.kind === MediaTypePhoto}>
-                    <MainPhoto photo={props.media as Photo} />
-                </Match>
-                <Match when={props.media.kind === MediaTypeVideo}>
-                    <MainVideo video={props.media as Video} videoSize={'large'} />
-                </Match>
-            </Switch>
-        </div>
+        <Show when={props.media}>
+            <div
+                class="h-100% w-100% max-h-100vh max-w-100% object-contain self-center"
+                style={`${props.maxHeightStyle ?? ''} ${getTransformStyles()} ${getFilterStyles()}`}>
+                <Switch>
+                    <Match when={props.media.kind === MediaTypePhoto}>
+                        <MainPhoto photo={props.media as Photo} />
+                    </Match>
+                    <Match when={props.media.kind === MediaTypeVideo}>
+                        <MainVideo video={props.media as Video} videoSize={'large'} />
+                    </Match>
+                </Switch>
+            </div>
+        </Show>
     );
 };
 
