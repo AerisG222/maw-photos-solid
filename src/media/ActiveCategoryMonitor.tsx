0bfa71ca19bd6f165aa4401ef607793d670ca35a
Mike Morano <mmorano@mikeandwan.us>
2023-07-27 07:13:09 -0400

    simplify media root and let category monitor set active category

--- src/media/ActiveCategoryMonitor.tsx
+++ src/media/ActiveCategoryMonitor.tsx
@@ -1,16 +1,22 @@
 import { ParentComponent, children, createEffect, onCleanup } from 'solid-js';
 
 import { useCategoryContext } from '../contexts/CategoryContext';
-import { CategoryTypePhotos, CategoryTypeVideos } from '../_models/CategoryType';
+import { CategoryType, CategoryTypePhotos, CategoryTypeVideos } from '../_models/CategoryType';
 import { useCategoryTeaserServiceContext } from './contexts/CategoryTeaserServiceContext';
 import { photoMediaService } from '../_services/media/PhotoMediaService';
 import { videoMediaService } from '../_services/media/VideoMediaService';
+import { useParams } from '@solidjs/router';
 
 const ActiveCategoryMonitor: ParentComponent = (props) => {
-    const [categoryContext, { setActiveCategory }] = useCategoryContext();
+    const [categoryContext, { setActiveCategory, setActiveCategoryById }] = useCategoryContext();
     const [, { setService: setCategoryTeaserService }] = useCategoryTeaserServiceContext();
-
     const c = children(() => props.children);
+    const params = useParams();
+
+    if(params.categoryType && params.categoryId) {
+        // dont monitor this in a reactive context, it will reload with each navigation to new category
+        setActiveCategoryById(params.categoryType as CategoryType, parseInt(params.categoryId, 10));
+    }
 
     createEffect(() => {
         switch(categoryContext.activeCategory?.type) {
--- src/media/MediaRoot.tsx
+++ src/media/MediaRoot.tsx
@@ -1,8 +1,7 @@
-import { Outlet, useParams } from '@solidjs/router';
+import { Outlet } from '@solidjs/router';
 import { Component } from 'solid-js';
 
 import { MediaListProvider } from './contexts/MediaListContext';
-import { CategoryType } from '../_models/CategoryType';
 import { SlideshowProvider } from './contexts/SlideshowContext';
 import { VisualEffectsProvider } from './contexts/VisualEffectsContext';
 import { CategoryTeaserServiceProvider } from './contexts/CategoryTeaserServiceContext';
@@ -10,7 +9,6 @@ import { CommentServiceProvider } from './contexts/CommentServiceContext';
 import { ExifServiceProvider } from './contexts/ExifServiceContext';
 import { MetadataEditServiceProvider } from './contexts/MetadataEditServiceContext';
 import { RatingServiceProvider } from './contexts/RatingServiceContext';
-import { useCategoryContext } from '../contexts/CategoryContext';
 
 import AuthGuard from '../components/auth/AuthGuard';
 import MediaLoader from './MediaLoader';
@@ -18,14 +16,6 @@ import ActiveMediaItem from './ActiveMediaItem';
 import ActiveCategoryMonitor from './ActiveCategoryMonitor';
 
 const MediaRoot: Component = () => {
-    const [, {setActiveCategoryById}] = useCategoryContext();
-    const params = useParams();
-
-    if(params.categoryType && params.categoryId) {
-        // dont monitor this in a reactive context, it will reload with each navigation to new category
-        setActiveCategoryById(params.categoryType as CategoryType, parseInt(params.categoryId, 10));
-    }
-
     return (
         <AuthGuard>
             <MediaListProvider>
@@ -34,17 +24,17 @@ const MediaRoot: Component = () => {
             <CommentServiceProvider>
             <MetadataEditServiceProvider>
             <CategoryTeaserServiceProvider>
-            <MediaLoader>
             <ActiveCategoryMonitor>
             <ActiveMediaItem>
+            <MediaLoader>
             <SlideshowProvider>
             <VisualEffectsProvider>
                 <Outlet />
             </VisualEffectsProvider>
             </SlideshowProvider>
+            </MediaLoader>
             </ActiveMediaItem>
             </ActiveCategoryMonitor>
-            </MediaLoader>
             </CategoryTeaserServiceProvider>
             </MetadataEditServiceProvider>
             </CommentServiceProvider>
