16820a61f274deb594f469ebe3e3f93926fc6ed6
Mike Morano <mmorano@mikeandwan.us>
2023-07-28 15:01:53 -0400

    example of multiple themes

--- src/_models/Theme.ts
+++ src/_models/Theme.ts
@@ -12,10 +12,20 @@ const themeDarkBlue: Record<string, string> = {
     'base-100':      '#15151f', // oklch(20% 0.02 284)
     'base-200':      '#20202b', // oklch(25% 0.02 284)
     'base-300':      '#2c2d38', // oklch(30% 0.02 284)
-}
+};
+
+const themeDarkGray: Record<string, string> = {
+    primary:         '#efb300', //
+    'primary-focus': '#c79512', // oklch(70% 0.14 84)
+
+    'base-100':      '#161616', // oklch(20% 0.00 0.00)
+    'base-200':      '#1f1f1f', // oklch(24% 0.00 0.00)
+    'base-300':      '#292929', // oklch(28% 0.00 0.00)
+};
 
 export const allThemes: Theme[] = [
-    { id: 'dark-blue', name: 'Dark Blue', def: themeDarkBlue }
+    { id: 'dark-blue', name: 'Dark Blue', def: themeDarkBlue },
+    { id: 'dark-gray', name: 'Dark Gray', def: themeDarkGray }
 ];
 
 export const getThemesForUno = (): Record<string, Record<string, string>>[] =>
