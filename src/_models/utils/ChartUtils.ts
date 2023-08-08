eb5ffbf0a0efc2a4b58e51a601501f89059963bb
Mike Morano <mmorano@mikeandwan.us>
2023-08-08 11:08:50 -0400

    try to improve chart colors

--- src/_models/utils/ChartUtils.ts
+++ src/_models/utils/ChartUtils.ts
@@ -1,42 +1,23 @@
 import { Category } from "../../_models/Category";
 
 // https://www.learnui.design/tools/data-color-picker.html
+// https://medialab.github.io/iwanthue/
 export const chartColors = [
-    "#003f5c",
-    "#2f4b7c",
-    "#665191",
-    "#a05195",
-    "#d45087",
-    "#f95d6a",
-    "#ff7c43",
-    "#ffa600",
-
-    "#5c1f1f",
-    "#772c24",
-    "#913c27",
-    "#ab4d28",
-    "#c36126",
-    "#da7621",
-    "#ee8d18",
-    "#ffa600",
-
-    "#0e5c00",
-    "#346900",
-    "#537600",
-    "#728200",
-    "#938d00",
-    "#b69700",
-    "#da9f00",
-    "#ffa600",
-
-    "#05005c",
-    "#550062",
-    "#89005f",
-    "#b40056",
-    "#d61449",
-    "#ee4c38",
-    "#fc7a22",
-    "#ffa600",
+    "#486ca6",
+    "#d93e92",
+    "#6ca3e0",
+    "#da4bc8",
+    "#4d7ddf",
+    "#a54d78",
+    "#5b5fde",
+    "#dc87c3",
+    "#9146d5",
+    "#815c97",
+    "#ac53c6",
+    "#5a55a8",
+    "#a04291",
+    "#af90e2",
+    "#845cbe"
 ];
 
 export const buildStatsData = (years: number[], categories: Category[], valueFunc: (cat: Category) => number) => {
