457117abf82cb104fcbae444c25de144b5fd841f
Mike Morano <mmorano@mikeandwan.us>
2023-08-09 16:31:49 -0400

    fix toolbar border on desktop

--- src/components/toolbar/ToolbarLayout.tsx
+++ src/components/toolbar/ToolbarLayout.tsx
@@ -5,7 +5,7 @@ const ToolbarLayout: ParentComponent = (props) => {
 
     return (
         <div class="flex bg-base-200  border-t-1 border-t-base-content:20%
-                md:flex-col md:border-r-1 md:border-r-base-content:20%">
+                md:flex-col md:border-t-0 md:border-r-1 md:border-r-base-content:20%">
             {c()}
         </div>
     );
