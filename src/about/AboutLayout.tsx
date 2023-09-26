bb7be076385cf3028c076c64692dc46155bed300
Mike Morano <mmorano@mikeandwan.us>
2023-09-26 16:59:04 -0400

    bump dependencies

--- src/about/AboutLayout.tsx
+++ src/about/AboutLayout.tsx
@@ -8,7 +8,7 @@ const AboutLayout: Component = () => {
     return (
         <Layout toolbar={<Toolbar />}>
             <div class="font-brand text-6xl text-center mt-4 md:my-8">
-                <img src="/icon.svg" width="156" height="156" class="display-inline mb-2" />
+                <img src="/icon.svg" width="156" height="156" class="inline mb-2" />
                 <h2 class="text-primary"><a href="https://www.mikeandwan.us">mikeandwan.us</a></h2>
                 <h2 class="text-secondary">Photos</h2>
                 <h2 class="text-secondary m-t-[-1rem]">v{import.meta.env.VITE_APP_VERSION}</h2>
