a6977e84adde5c63cd81bfd4ae4beec64791c8d4
Mike Morano <mmorano@mikeandwan.us>
2023-10-11 06:56:54 -0400

    update release info

--- src/about/ViewReleaseNotes.tsx
+++ src/about/ViewReleaseNotes.tsx
@@ -5,6 +5,17 @@ const ViewApplication: Component = () => {
         <>
             <h1 class="head1">Release Notes</h1>
 
+            <h3 class="head3"><span class="version">v3.0.2</span> : <span class="date">2023-10-11</span></h3>
+            <p>
+                A handful of improvements:
+            </p>
+            <ul class="list-inside list-disc mb-4">
+                <li>Improve how the category filter is applied to the main category listing page</li>
+                <li>Update bulk edit tool to update categories once all media items have GPS data</li>
+                <li>Draw histograms when media item was already loaded</li>
+                <li>A number of minor styling improvements</li>
+            </ul>
+
             <h3 class="head3"><span class="version">v3.0.1</span> : <span class="date">2023-08-26</span></h3>
             <p>
                 Fix an issue preventing some users from accessing photos.
