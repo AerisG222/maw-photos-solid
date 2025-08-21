6641c2dfa9e9937ba5a5d926ec29ec0ad240cbc6
Mike Morano <mmorano@mikeandwan.us>
2025-08-21 19:20:04 -0400

    constrain infocard height

--- src/_components/sidebar/InfoCard.tsx
+++ src/_components/sidebar/InfoCard.tsx
@@ -9,7 +9,7 @@ const InfoCard: ParentComponent<Props> = props => {
     const c = children(() => props.children);
 
     return (
-        <div class="bg-base-300 rounded m-1 p-2 border-1 border-secondary/20">
+        <div class="bg-base-300 rounded m-1 p-2 border-1 border-secondary/20 max-h-164 overflow-y-scroll">
             <div class="flex flex-items-center text-secondary">
                 <span class={`block mr-2 text-6 ${props.icon}`} />
                 <span class="block text-sm font-bold">{props.title}</span>
