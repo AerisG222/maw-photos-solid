5e301c6b496ba9927cce3d7a4cb618767d1f849d
Mike Morano <mmorano@mikeandwan.us>
2025-08-24 16:56:41 -0400

    minor cleanup

--- src/search/components/SearchMoreButton.tsx
+++ src/search/components/SearchMoreButton.tsx
@@ -8,7 +8,7 @@ const SearchMoreButton: Component<Props> = props => {
     return (
         <div class="flex justify-center my-3">
             <button class="btn btn-primary btn-outline" onClick={() => props.continueSearch()}>
-                <span class="text-6 icon-[ic--round-keyboard-arrow-down]" /> Show More
+                <span class="text-lg icon-[ic--round-keyboard-arrow-down]" /> Show More
             </button>
         </div>
     );
