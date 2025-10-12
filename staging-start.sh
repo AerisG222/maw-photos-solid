1b678e64c9cdba9a14172401d506a5b1ef96f255
Mike Morano <mmorano@mikeandwan.us>
2025-10-11 20:31:16 -0400

    cleanups when testing via gateway

--- staging-start.sh
+++ staging-start.sh
@@ -3,5 +3,5 @@
 podman run --rm \
     --name staging-maw-photos \
     --publish "4200:4200" \
-    --volume "/home/mmorano/maw-photos/staging/certificates/:/certs:ro,Z" \
+    --volume "/home/mmorano/maw-photos/staging/photos-secrets:/certs:ro,Z" \
     localhost/maw-photos-staging
